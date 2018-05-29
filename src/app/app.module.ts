f750d4573aa900be2ffa1fd4ccb53e3af59e6443
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-29 20:08:23 +0700

    word form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WordComponent } from './word/word.component';
 import { ListWordComponent } from './list-word/list-word.component';
+import { WordFormComponent } from './components/word-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
-    ListWordComponent
+    ListWordComponent,
+    WordFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/word-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-word-form',
+    template: `
+        <h4>Word Form Component</h4>
+    `
+})
+
+export class WordFormComponent {}
