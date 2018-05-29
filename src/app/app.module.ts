a19ebdd9ab76c7aaaaba3a40851c2c4a0185cca3
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-29 19:57:31 +0700

    list word

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WordComponent } from './word/word.component';
+import { ListWordComponent } from './list-word/list-word.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordComponent,
+    ListWordComponent
   ],
   imports: [
     BrowserModule
