74f8678b0b4ad0b1c0be36c2b7d926ac52711b5e
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-29 20:49:09 +0700

    list word

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ListWordComponent } from './list-word/list-word.component';
+import { ListWordComponent } from './components/list-word.component';
 
 import { WordFormComponent } from './components/word-form.component';
 import { WordComponent } from './components/word.component';
---
+++ src/app/components/list-word.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-word',
+  template: `
+    <h3>List Word Component</h3>
+  `
+})
+
+export class ListWordComponent {
+}
