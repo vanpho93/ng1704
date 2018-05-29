21c2cc962c2ad1ff8950968070e8d426d1bd15e2
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-29 20:22:55 +0700

    change value

--- src/app/components/word.component.ts
+++ src/app/components/word.component.ts
@@ -4,8 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-word',
   template: `
     <h3>Word Component</h3>
+    <h4>Count = {{ count }}</h4>
+    <button class="btn btn-success" (click)="increase();">Increase</button>
+    <button class="btn btn-danger" (click)="descrease();">Descrease</button>
+    <button class="btn btn-warning" (click)="reset();">Reset</button>
   `
 })
 
 export class WordComponent {
+  count = 1;
+
+  increase() { this.count++; }
+  descrease() { this.count--; }
+  reset() { this.count = 1; }
 }
