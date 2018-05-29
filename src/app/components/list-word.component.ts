269e9444658fb35664b92923f9778d1641205819
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-29 21:05:12 +0700

    app-list-word

--- src/app/components/list-word.component.ts
+++ src/app/components/list-word.component.ts
@@ -5,8 +5,32 @@ import { Word } from '../types';
   selector: 'app-list-word',
   template: `
     <h3>List Word Component</h3>
-    <pre>{{ words | json }}</pre>
-  `
+    <div class="word">
+      <div class="word-container">
+        <h3 class="text-success">One</h3>
+        <h3 class="text-danger">Mot</h3>
+      </div><div class="btn-container">
+        <button class="btn btn-danger">Memorized</button>
+        <button class="btn btn-warning">Remove</button>
+      </div>
+    </div>
+  `,
+  styles: [`
+      .word {
+        width: 200px;
+        display: flex;
+        flex-direction: column;
+        background-color: #F0F0F0;
+        border-radius: 5px;
+        padding: 10px;
+        margin-top: 10px;
+    }
+    .word-container, .btn-container {
+        align-items: flex-end;
+        display: flex;
+        justify-content: space-around;
+    }
+  `]
 })
 
 export class ListWordComponent {
