1b7b9b41933724085027883a927539bc84739bbf
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-29 20:03:04 +0700

    template and styles

--- src/app/word/word.component.ts
+++ src/app/word/word.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-word',
-  templateUrl: './word.component.html',
+  // templateUrl: './word.component.html',
+  template: `
+    <h3>Word Component</h3>
+  `,
+  // styles: [`h3 { color: green; }`]
   styleUrls: ['./word.component.css']
 })
 export class WordComponent implements OnInit {
-
-  constructor() { }
 
   ngOnInit() {
   }
