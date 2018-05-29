72dc86dcfd782c65004b28e7125677ac067c8eac
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-29 20:43:19 +0700

    button

--- src/app/components/word.component.ts
+++ src/app/components/word.component.ts
@@ -4,14 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-word',
   template: `
     <h3>Word Component</h3>
-    <h4>Count = {{ count }}</h4>
+    <h4 class="{{ count % 2 === 0 ? 'text-danger' : 'text-success' }}">
+      Count = {{ count }}
+    </h4>
     <button class="btn btn-success" (click)="increase();">Increase</button>
     <button class="btn btn-danger" (click)="descrease();">Descrease</button>
     <button class="btn btn-warning" (click)="reset();">Reset</button>
     <hr>
     <div class="word">
-      <h3>{{ word.en }}</h3>
+      <h3 class="{{ word.isMemorized ? 'text-danger' : 'text-success' }}">
+        {{ word.en }}
+      </h3>
       <p>{{ word.vn }}</p>
+      <button class="{{ word.isMemorized ? 'btn btn-success': 'btn btn-danger' }}" (click)="toggle();">
+        {{ word.isMemorized ? 'Forgot': 'Memorized' }}
+      </button>
     </div>
   `
 })
@@ -23,6 +30,7 @@ export class WordComponent {
   increase() { this.count++; }
   descrease() { this.count--; }
   reset() { this.count = 1; }
+  toggle() { this.word.isMemorized = !this.word.isMemorized; }
 }
 
 interface Word {
