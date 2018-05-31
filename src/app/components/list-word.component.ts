3dd97bbf44aa8a878fa737da3fce80e03422362c
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-31 19:51:32 +0700

    form

--- src/app/components/list-word.component.ts
+++ src/app/components/list-word.component.ts
@@ -5,6 +5,20 @@ import { Word } from '../types';
   selector: 'app-list-word',
   template: `
     <h3>List Word Component</h3>
+    <div class="word form-group" style="width: 200px">
+        <input placeholder="English" class="form-control" [(ngModel)]="txtEn"/>
+        <br />
+        <input placeholder="Vietnamese" class="form-control" [(ngModel)]="txtVn"/>
+        <br />
+        <div class="btn-container">
+            <button class="btn btn-success" (click)="addWord();">
+                Add word
+            </button>
+            <button class="btn btn-danger">
+                Cancel
+            </button>
+        </div>
+    </div>
     <div class="word" *ngFor="let word of words">
       <div class="word-container">
         <h3 class="text-success">{{ word.en }}</h3>
