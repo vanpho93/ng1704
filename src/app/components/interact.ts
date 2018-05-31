8003bd83ebb3a2ed08d69dd6734b7381f34c026d
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-31 21:01:45 +0700

    output 1

--- src/app/components/interact.ts
+++ src/app/components/interact.ts
@@ -9,24 +9,24 @@ import { Component, Output, EventEmitter } from '@angular/core';
         (onIncrease)="onIncrease();"
         (onDecrease)="onDecrease();"
         (onReset)="onReset();"
+        (onChange)="onChange($event);"
         ></app-child>
   `
 })
 
 export class ParentComponent {
     count = 1;
+    onChange(isIncrease: boolean) { isIncrease ? this.count++ : this.count--; }
     onIncrease() { this.count++; }
     onDecrease() { this.count--; }
     onReset() { this.count = 1; }
 }
-
 // ----------------------------------
-
 @Component({
     selector: 'app-child',
     template: `
-        <button class="btn btn-success" (click)="onIncrease.emit();">Increase</button>
-        <button class="btn btn-danger" (click)="onDecrease.emit();">Decrease</button>
+        <button class="btn btn-success" (click)="onChange.emit(true);">Increase</button>
+        <button class="btn btn-danger" (click)="onChange.emit(false);">Decrease</button>
         <button class="btn btn-warning" (click)="onReset.emit();">Reset</button>
     `
 })
@@ -35,4 +35,5 @@ export class ChildComponent {
     @Output() onIncrease = new EventEmitter<void>();
     @Output() onDecrease = new EventEmitter<void>();
     @Output() onReset = new EventEmitter<void>();
+    @Output() onChange = new EventEmitter<boolean>();
 }
