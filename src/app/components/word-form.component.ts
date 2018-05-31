0d991ffa921d8d0382c391f9f8e18d91b7491068
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-31 19:43:13 +0700

    form

--- src/app/components/word-form.component.ts
+++ src/app/components/word-form.component.ts
@@ -4,6 +4,20 @@ import { Component } from '@angular/core';
     selector: 'app-word-form',
     template: `
         <h4>Word Form Component</h4>
+        <div class="form-group" style="width: 200px">
+            <input placeholder="English" class="form-control" />
+            <br />
+            <input placeholder="Vietnamese" class="form-control" />
+            <br />
+            <div class="btn-container">
+                <button class="btn btn-success">
+                    Add word
+                </button>
+                <button class="btn btn-danger">
+                    Cancel
+                </button>
+            </div>
+        </div>
     `
 })
 
