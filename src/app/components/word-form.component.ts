8d2a16bb30f0f85b07dc6f6329a2a18e81e441d5
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-31 19:49:27 +0700

    xong

--- src/app/components/word-form.component.ts
+++ src/app/components/word-form.component.ts
@@ -5,20 +5,28 @@ import { Component } from '@angular/core';
     template: `
         <h4>Word Form Component</h4>
         <div class="form-group" style="width: 200px">
-            <input placeholder="English" class="form-control" />
+            <input placeholder="English" class="form-control" [(ngModel)]="txtEn"/>
             <br />
-            <input placeholder="Vietnamese" class="form-control" />
+            <input placeholder="Vietnamese" class="form-control" [(ngModel)]="txtVn"/>
             <br />
             <div class="btn-container">
-                <button class="btn btn-success">
+                <button class="btn btn-success" (click)="addWord();">
                     Add word
                 </button>
                 <button class="btn btn-danger">
                     Cancel
                 </button>
             </div>
+            <pre>en: {{ txtEn }} | vn: {{ txtVn }}</pre>
         </div>
     `
 })
 
-export class WordFormComponent {}
+export class WordFormComponent {
+    txtEn = '';
+    txtVn = '';
+
+    addWord() {
+        alert(`en: ${this.txtEn}, vn: ${this.txtVn}`);
+    }
+}
