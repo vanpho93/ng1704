29057e7534c6164059a8378483342da5e50e010a
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-02 20:22:47 +0700

    count

--- src/app/components/interact.ts
+++ src/app/components/interact.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-parent',
   template: `
     <h3>Parent Component</h3>
-    <h4>Count = 0</h4>
+    <h4>Count = {{ myCount }}</h4>
     <app-child></app-child>
   `
 })
 
-export class ParentComponent {}
+export class ParentComponent {
+  myCount: number;
+  constructor(private store: Store<{ count: number }>) {
+    this.store.select('count').subscribe(count => this.myCount = count);
+  }
+}
 // ----------------------------------
 @Component({
     selector: 'app-child',
     template: `
-        <button class="btn btn-success">Increase</button>
-        <button class="btn btn-danger">Decrease</button>
-        <button class="btn btn-warning">Reset</button>
+        <button class="btn btn-success" (click)="increase();">Increase</button>
+        <button class="btn btn-danger" (click)="decrease();">Decrease</button>
+        <button class="btn btn-warning" (click)="reset();">Reset</button>
     `
 })
 
-export class ChildComponent {}
+export class ChildComponent {
+  constructor(private store: Store<{ count: number }>) {}
+
+  increase() { this.store.dispatch({ type: 'INCREASE' }); }
+  decrease() { this.store.dispatch({ type: 'DECREASE' }); }
+  reset() { this.store.dispatch({ type: 'RESET' }); }
+}
