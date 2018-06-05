7c81675464e5a9da39d9c98ee0e650b9771ad306
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-05 21:16:08 +0700

    my form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { StoreModule } from '@ngrx/store';
 
@@ -37,6 +37,7 @@ import { RoundPipe } from './pipes/round.pipe';
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     StoreModule.forRoot({
       count: countReducer,
       words: wordsReducer,
--- src/app/components/sign-up.component.ts
+++ src/app/components/sign-up.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   template: `
-  <form class="form-group" style="width: 200px; margin-top: 10px">
+  <form class="form-group" style="width: 200px; margin-top: 10px" [formGroup]="formSignUp">
     <input
       class="form-control"
       placeholder="Email"
+      formControlName="email"
     />
     <br>
     <input
       class="form-control"
       placeholder="Password"
+      formControlName="password"
       type="password"
     />
     <br>
@@ -19,7 +22,17 @@ import { Component } from '@angular/core';
       Sign In
     </button>
   </form>
+  <pre>{{ formSignUp.value | json }}</pre>
   `,
 })
 
-export class SignUpComponent { }
+export class SignUpComponent {
+  formSignUp: FormGroup;
+
+  constructor() {
+    this.formSignUp = new FormGroup({
+      email: new FormControl('teo@gmail.com'),
+      password: new FormControl('123')
+    });
+  }
+}
