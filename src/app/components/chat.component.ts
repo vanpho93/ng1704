2b9a62fc5523b9865d29c489b199d24d40d8cd5d
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-05 20:19:00 +0700

    client

--- src/app/components/chat.component.ts
+++ src/app/components/chat.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-chat',
   template: `
     <div class="container" style="margin-top: 10px">
-      <input class="form-control" placeholder="Enter your message" />
+      <input class="form-control" placeholder="Enter your message" [(ngModel)]="txtMessage" />
       <br>
-      <button class="btn btn-success">Send Message</button>
+      <button class="btn btn-success" (click)="sendMessage()">Send Message</button>
     </div>
   `
 })
 
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+  txtMessage = '';
   socket = io('http://localhost:4000');
+
+  ngOnInit() {
+    this.socket.on('SERVER_SEND_MESSAGE', message => console.log(message));
+  }
+
+  sendMessage() {
+    this.socket.emit('CLIENT_SEND_MESSAGE', this.txtMessage);
+    this.txtMessage = '';
+  }
 }
